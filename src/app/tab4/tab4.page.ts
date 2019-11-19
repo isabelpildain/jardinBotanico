8ed096bace531a8c5f86b08993b7a5ae52432b0f
501 <501@academicos.cesde.edu.co>
2019-11-19 13:56:38 -0500

    abrir archivo pdf

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {ServicioInterface} from '../../interfaces/ServicioInterface';
 import {ParseProviderService} from '../parse-provider.service';
 
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
+
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
@@ -11,7 +13,7 @@ export class Tab4Page {
 
   itemsServicios: ServicioInterface[] = [];
 
-  constructor(private parseProvider: ParseProviderService) {
+  constructor(private parseProvider: ParseProviderService, private iab: InAppBrowser) {
     this.listServicios();
   }
 
@@ -25,5 +27,10 @@ export class Tab4Page {
     }, (error) => {
       console.log(error);
     });
+  }
+
+  openUrl(){
+   
+    this.iab.create('https://yofasele.sirv.com/Images/jardinbotanico/Portafolios_digitales_JBM_2019_/Artes%20Portafolio%201%20Silvicultura%20y%20Paisajismo%20DIGITAL.pdf','_self')
   }
 }
