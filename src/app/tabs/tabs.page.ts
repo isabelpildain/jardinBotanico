e5c3a7a7faf5aa55b533913f476d41bd71079e5c
501 <501@academicos.cesde.edu.co>
2019-11-19 13:23:12 -0500

    nuevas actividades

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -28,15 +28,13 @@ export class TabsPage {
 
 }
 
-export class RefresherExample {
+export class Refresher {
   constructor() {}
 
   doRefresh(event) {
-    console.log('Begin async operation');
 
     setTimeout(() => {
-      console.log('Async operation has ended');
       event.target.complete();
-    }, 2000);
+    }, 800);
   }
 }
