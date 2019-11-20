b603b0616af2e2511b315dfce9cd7f44623fe02c
501 <501@academicos.cesde.edu.co>
2019-11-20 13:40:02 -0500

    nuevos cambios de imagenes

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -3,7 +3,6 @@ import {ServicioInterface} from '../../interfaces/ServicioInterface';
 import {ParseProviderService} from '../parse-provider.service';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
-import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-tab4',
@@ -18,10 +17,6 @@ export class Tab4Page {
   constructor(private parseProvider: ParseProviderService, private iab: InAppBrowser) {
     this.listServicios();
   }
-  openUrl(){
-    // this.browser.create('https://www.botanicomedellin.org/servicios/ciencia-y-biodiversidad/silvicultura-y-paisajismo/', '_self')
-  }
-
   public listServicios(): Promise<any> {
     const offset = this.itemsServicios.length;
     const limit = 10;
