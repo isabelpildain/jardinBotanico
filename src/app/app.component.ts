76f97290af8a8ef380182f51dc8bfd2a2a2813db
alejomorales24 <57499646+alejomorales24@users.noreply.github.com>
2019-11-28 16:09:35 -0500

    icon y splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,12 +56,12 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
 
-      timer(3000).subscribe(()=> this.showSplash = false)
+      timer(3000).subscribe(() => this.showSplash = false)
 
       Environment.setEnv({
         'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDGODufigYaWvP-Lg2nmzoRkKd3QbqUsR0',
         'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDGODufigYaWvP-Lg2nmzoRkKd3QbqUsR0'
-        
+
       });
 
       this.statusBar.styleDefault();
